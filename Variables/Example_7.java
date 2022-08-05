89336c13f77dc4a82223a64f898f2ff1817795e5
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 01:41:16 +0200

    Variables - Example 7

---
+++ Variables/Example_7.java
@@ -0,0 +1,11 @@
+//Add a variable to another variable
+
+public class Example_7 {
+    public static void main(String[] args) {
+        String firstName = "Luis ";
+        String lastName = "Mendoza";
+
+        String fullName = firstName + lastName;
+        System.out.println(fullName);  
+    }
+}
