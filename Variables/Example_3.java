9c0444bd3f827db1d7fe6398f81245a01ba69f63
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 01:31:01 +0200

    Variables - Example 3

---
+++ Variables/Example_3.java
@@ -0,0 +1,10 @@
+//Create a variable without assigning the value, and assign the value later
+
+public class Example_3 {
+    public static void main(String[] args) {
+        int myNum;
+
+        myNum = 15;
+        System.out.println(myNum);
+    }
+}
