cd5d33d96be6578a64007bfb26b6764baa86ed56
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 01:40:33 +0200

    Variables - Example 6

---
+++ Variables/Example_6.java
@@ -0,0 +1,9 @@
+//Combine text and a variable on display
+
+public class Example_6 {
+    public static void main(String[] args) {
+        String name = "Luis";
+
+        System.out.println("Hello " + name);
+    }
+}
