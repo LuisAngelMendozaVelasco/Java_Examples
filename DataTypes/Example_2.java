f681f35e959d22be18a280d1e2ac3b477c5e74c0
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 01:58:23 +0200

    DataTypes - Example 2

---
+++ DataTypes/Example_2.java
@@ -0,0 +1,9 @@
+//Create a byte type
+
+public class Example_2 {
+    public static void main(String[] args) {
+        byte myNum = 100; // Stores whole numbers from -128 to 127
+
+        System.out.println(myNum);  
+    }
+}
