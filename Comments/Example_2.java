fb3b6ac1dcf0e42b17e46491214efb359b796192
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 01:26:41 +0200

    Comments - Example 2

---
+++ Comments/Example_2.java
@@ -0,0 +1,7 @@
+//Single-line comment at the end of a line code
+
+public class Example_2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // This is a comment
+    }
+}
