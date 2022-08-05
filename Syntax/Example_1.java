579981095d6ece0b47270d9b11dbc521fc406093
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 01:15:10 +0200

    Syntax - Example 1

---
+++ Syntax/Example_1.java
@@ -0,0 +1,16 @@
+/*Every line of code that runs in Java must be inside a class.
+A class should always start with an uppercase first letter.
+Note: Java is case-sensitive: "MyClass" and "myclass" has different meaning.
+The name of the java file must match the class name. When saving the file, save it using the class name and add ".java" to the end of the filename.*/
+
+public class Example_1 {
+    /*The main() method is required and you will see it in every Java program
+    Any code inside the main() method will be executed. Don't worry about the keywords before and after main. You will get to know them bit by bit while reading this tutorial.
+    For now, just remember that every Java program has a class name which must match the filename, and that every program must contain the main() method.*/
+    public static void main(String[] args) { //The curly braces {} marks the beginning and the end of a block of code.
+        /*Inside the main() method, we can use the println() method to print a line of text to the screen
+        System is a built-in Java class that contains useful members, such as out, which is short for "output". 
+        The println() method, short for "print line", is used to print a value to the screen (or a file).*/
+        System.out.println("Hello World"); //Each code statement must end with a semicolon (;).
+    }
+}
