df885e6ad9e39e7417272acd5dada884091fd2b9
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 01:16:04 +0200

    Comments - Example 1

---
+++ Comments/Example_1.java
@@ -0,0 +1,8 @@
+//Single-line comment before a line of code
+
+public class Example_1 {
+    public static void main(String[] args) {
+        // This is a comment
+        System.out.println("Hello World");
+    }
+}
