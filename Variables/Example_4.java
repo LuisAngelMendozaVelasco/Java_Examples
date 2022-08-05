9d04687143f0cae079fdd54a53201209d4196dc9
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 01:33:26 +0200

    Variables - Example 4

---
+++ Variables/Example_4.java
@@ -0,0 +1,10 @@
+//Overwrite an existing variable value
+
+public class Example_4 {
+    public static void main(String[] args) {
+        int myNum = 15;
+
+        myNum = 20;  // myNum is now 20
+        System.out.println(myNum);
+    }
+}
