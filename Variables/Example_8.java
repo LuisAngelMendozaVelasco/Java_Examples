5e053b3064bc65c1cd80f73ecd35558be7f3e75e
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 01:43:07 +0200

    Variables - Example 8

---
+++ Variables/Example_8.java
@@ -0,0 +1,9 @@
+//Declare many variables of the same type with a comma-separated list
+
+public class Example_8 {
+    public static void main(String[] args) {
+        int x = 5, y = 6, z = 50;
+
+        System.out.println(x + y + z);
+    }
+}
