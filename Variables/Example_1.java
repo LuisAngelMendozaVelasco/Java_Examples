e5ca881d2924e618a353b0e8919a43521ef53c8d
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 01:27:42 +0200

    Variables - Example 1

---
+++ Variables/Example_1.java
@@ -0,0 +1,9 @@
+//Create a string variable
+
+public class Example_1 {
+    public static void main(String[] args) {
+        String name = "John";
+        
+        System.out.println(name);
+    }
+}
