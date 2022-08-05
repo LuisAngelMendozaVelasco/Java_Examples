4f762653d0546ba42c1198cdcc98362469d01229
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 01:58:58 +0200

    DataTypes - Example 3

---
+++ DataTypes/Example_3.java
@@ -0,0 +1,9 @@
+//Create a short type
+
+public class Example_3 {
+    public static void main(String[] args) {
+        short myNum = 5000; // Stores whole numbers from -32,768 to 32,767
+
+        System.out.println(myNum);  
+    }
+}
