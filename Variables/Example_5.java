0220f9ccd36f8bd2d58076344f8c763c6f0ac3c6
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 01:36:29 +0200

    Variables - Example 5

---
+++ Variables/Example_5.java
@@ -0,0 +1,10 @@
+//Create a final variable (unchangeable and read-only)
+
+public class Example_5 {
+    public static void main(String[] args) {
+        final int myNum = 15;
+
+        myNum = 20; // will generate an error
+        System.out.println(myNum);
+    }
+}
