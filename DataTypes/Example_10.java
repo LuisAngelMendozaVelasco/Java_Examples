2b6605f752105ff0a0c080174d0399de5b2972b1
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 02:11:55 +0200

    DataTypes - Example 10

---
+++ DataTypes/Example_10.java
@@ -0,0 +1,9 @@
+//Create a String type
+
+public class Example_10 {
+    public static void main(String[] args) {
+        String greeting = "Hello World";
+
+        System.out.println(greeting);
+    }
+}
