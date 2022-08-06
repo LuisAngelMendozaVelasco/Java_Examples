e20af0e6ecc79a8bd63cf90af07a37a95a302e94
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 17:08:40 +0200

    Loops - Example_2

---
+++ Loops/Example_2.java
@@ -0,0 +1,13 @@
+//Do while loop
+
+public class Example_2 {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while(i < 5);  
+    }
+}
