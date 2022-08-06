79d51e4ec1595e46cfe7a9f770f71d26155bf96c
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 17:24:47 +0200

    Arrays - Example 2

---
+++ Arrays/Example_2.java
@@ -0,0 +1,10 @@
+//Change an array element
+
+public class Example_2 {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        
+        cars[0] = "Opel";
+        System.out.println(cars[0]);
+    }
+}
