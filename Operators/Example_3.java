66aa9e1296f36697c576fc9b43a73b74bcf768f8
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 03:24:03 +0200

    Operators - Example 3

---
+++ Operators/Example_3.java
@@ -0,0 +1,10 @@
+//Multiplication operator
+
+public class Example_3 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+
+        System.out.println(x * y);
+    }
+}
