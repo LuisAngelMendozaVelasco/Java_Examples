635d7153fecf7b9f0eec2786c61669f9e5e01786
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 04:07:57 +0200

    Math - Example 5

---
+++ Math/Example_5.java
@@ -0,0 +1,7 @@
+//Math.random() - return a random number between 0 and 
+
+public class Example_5 {
+    public static void main(String[] args) {
+        System.out.println(Math.random());  
+    }
+}
