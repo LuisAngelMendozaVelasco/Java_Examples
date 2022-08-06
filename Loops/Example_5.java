349dca4358cc16890a0cb239da2fa10a05dda429
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 17:16:15 +0200

    Loops - Example_5

---
+++ Loops/Example_5.java
@@ -0,0 +1,12 @@
+//Break a loop
+
+public class Example_5 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            if(i == 4) {
+                break;
+            }
+            System.out.println(i);
+        }  
+    }
+}
