7110b40392ad239470bf8dd9d199cf54a6b4827b
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 03:38:23 +0200

    Strings - Example 3

---
+++ Strings/Example_3.java
@@ -0,0 +1,9 @@
+//Find the length of a string
+
+public class Example_3 {
+    public static void main(String[] args) {
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        System.out.println("The length of the txt string is: " + txt.length());
+    }
+}
