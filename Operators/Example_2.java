08263c7ae9f33f444292fdb2a859594bb91b2f62
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 03:22:35 +0200

    Operators - Example 2

---
+++ Operators/Example_2.java
@@ -0,0 +1,10 @@
+//Subtraction operator
+
+public class Example_2 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        
+        System.out.println(x - y);
+    }
+}
