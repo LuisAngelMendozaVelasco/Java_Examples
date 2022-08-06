3e3198a5269831619877c4571726d82fa064ec0e
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 21:42:02 +0200

    Methods - Example 3

---
+++ Methods/Example_3.java
@@ -0,0 +1,13 @@
+//Method with parameters
+
+public class Example_3 {
+    static void myMethod(String fname) {
+        System.out.println(fname + " Refsnes");
+    }
+    
+    public static void main(String[] args) {
+        myMethod("Liam");
+        myMethod("Jenny");
+        myMethod("Anja");
+    }
+}
