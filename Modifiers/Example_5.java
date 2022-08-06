c27e80e0e2bf51b449637b7495f9dd7a7f0c9cf1
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 01:02:53 +0200

    Modifiers - Example 5

---
+++ Modifiers/Example_5.java
@@ -0,0 +1,17 @@
+//Default attributes
+
+public class Example_5 {
+    // default	The code is only accessible in the same package. This is used when you don't specify a modifier.
+    String fname = "Luis";
+    String lname = "Mendoza";
+    String email = "lluis@mendoza.com";
+    int age = 24;
+    
+    public static void main(String[] args) {
+        Example_5 myObj = new Example_5();
+
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+        System.out.println("Email: " + myObj.email);
+        System.out.println("Age: " + myObj.age);
+    }
+}
