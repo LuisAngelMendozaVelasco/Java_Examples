77f4a093d04794a27cb374bc40492142fef9598a
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 19:07:31 +0200

    Methods - Example 1

---
+++ Methods/Example_1.java
@@ -0,0 +1,15 @@
+//Create and call a method
+
+/*myMethod() is the name of the method
+static means that the method belongs to the Main class and not an object of the Main class.
+void means that this method does not have a return value.*/
+
+public class Example_1 {
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+    
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
