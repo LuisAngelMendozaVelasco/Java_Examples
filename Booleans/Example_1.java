1c79f3ccd544f1f22e12c22dac087d69b3131f44
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 04:14:49 +0200

    Update Booleans - Example 1
    Add description

--- Booleans/Example_1.java
+++ Booleans/Example_1.java
@@ -1,10 +1,10 @@
-package Booleans;
+//Create a boolean type
 
 public class Example_1 {
     public static void main(String[] args) {
         boolean isJavaFun = true;
         boolean isFishTasty = false;   
-         
+
         System.out.println(isJavaFun);
         System.out.println(isFishTasty);
     }
