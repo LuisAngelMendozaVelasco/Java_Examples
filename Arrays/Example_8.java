799dabef1c1458435c513fbf98301e9bf6c7131d
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 17:40:13 +0200

    Arrays - Example 8

---
+++ Arrays/Example_8.java
@@ -0,0 +1,13 @@
+//Loop through a multidimensional array
+
+public class Example_8 {
+    public static void main(String[] args) {
+        int[][] myNumbers = {{1, 2, 3, 4}, {5, 6, 7}};
+
+        for(int i = 0; i < myNumbers.length; ++i) {
+            for(int j = 0; j < myNumbers[i].length; ++j) {
+                System.out.println(myNumbers[i][j]);
+            }
+        }
+    }
+}
