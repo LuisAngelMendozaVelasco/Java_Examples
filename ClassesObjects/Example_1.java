902d7f0d36056d80239a8a8404d2ebb1a36b036f
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 22:35:02 +0200

    ClassesObjects - Example 1

---
+++ ClassesObjects/Example_1.java
@@ -0,0 +1,11 @@
+//Create a class and an object of a class
+
+public class Example_1 {
+    int x = 5;
+
+    public static void main(String[] args) {
+        Example_1 myObj = new Example_1();
+
+        System.out.println(myObj.x);
+    }
+}
