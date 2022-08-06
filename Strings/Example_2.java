277095866590381b9b78c8ce2b94c970d0917dc1
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 03:37:05 +0200

    Strings - Example 2

---
+++ Strings/Example_2.java
@@ -0,0 +1,11 @@
+//Quotes inside a string
+
+public class Example_2 {
+    public static void main(String[] args) {
+        String txt1 = "It's alright";
+        String txt2 = "That's great";
+        
+        System.out.println(txt1);
+        System.out.println(txt2);
+    }
+}
