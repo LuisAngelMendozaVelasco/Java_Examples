d173b655aee7bd6fb8fd9aaf89ff6bd971a4159b
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 17:26:39 +0200

    Arrays - Example 3

---
+++ Arrays/Example_3.java
@@ -0,0 +1,9 @@
+//Find the length of an array
+
+public class Example_3 {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        
+        System.out.println(cars.length);
+    }
+}
