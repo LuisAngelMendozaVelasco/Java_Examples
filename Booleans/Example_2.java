a3d00796d684117184e0062fef3aea21acd07d2d
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 04:16:34 +0200

    Booleans - Example 2

---
+++ Booleans/Example_2.java
@@ -0,0 +1,10 @@
+//Find out if an expression is true or false
+
+public class Example_2 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 9;
+        
+        System.out.println(x > y); // returns true, because 10 is higher than 9  
+    }
+}
