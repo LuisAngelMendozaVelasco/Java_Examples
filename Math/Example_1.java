887027732c4923b9a491d335e439b1b7944edb36
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 03:49:53 +0200

    Math - Example 1

---
+++ Math/Example_1.java
@@ -0,0 +1,7 @@
+//Math.max(x,y) - return the highest value of x and y
+
+public class Example_1 {
+    public static void main(String[] args) {
+        System.out.println(Math.max(5, 10));  
+    }
+}
