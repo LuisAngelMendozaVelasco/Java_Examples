3d8d3511765e7a90125a0088ca662f6a547f2f98
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 03:42:29 +0200

    Strings - Example 5

---
+++ Strings/Example_5.java
@@ -0,0 +1,9 @@
+//Finding a string in a string
+
+public class Example_5 {
+    public static void main(String[] args) {
+        String txt = "Please locate where 'locate' occurs!";
+        
+        System.out.println(txt.indexOf("locate"));
+    }
+}
