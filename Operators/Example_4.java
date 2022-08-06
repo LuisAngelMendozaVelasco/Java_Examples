bc63bfd2ff7375f3d3ec3e6515e3b8552284746a
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 03:25:29 +0200

    Operators - Example 4

---
+++ Operators/Example_4.java
@@ -0,0 +1,10 @@
+//Division operator
+
+public class Example_4 {
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 3;
+
+        System.out.println(x / y);
+    }
+}
