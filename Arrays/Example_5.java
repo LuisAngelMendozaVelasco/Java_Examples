76f8ecb83a4953caaf745c7cdeeadd71ee7a29fd
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 17:33:41 +0200

    Arrays - Example 5

---
+++ Arrays/Example_5.java
@@ -0,0 +1,11 @@
+//Loop through an array
+
+public class Example_5 {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        for(int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+    }
+}
