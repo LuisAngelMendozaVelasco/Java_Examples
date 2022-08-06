491e800f864f2be2ca4f6bd383f56c3a8bceebe8
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 04:05:39 +0200

    Math - Example 4

---
+++ Math/Example_4.java
@@ -0,0 +1,7 @@
+//Math.abs(x) - return the absolute (positive) value of x
+
+public class Example_4 {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-4.7));  
+    }
+}
