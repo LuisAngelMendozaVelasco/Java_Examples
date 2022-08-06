5b4c36673918cc01e3de7c44b7b2e5b8b7fcf722
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 01:29:50 +0200

    Modifiers - Example 7

---
+++ Modifiers/Example_7.java
@@ -0,0 +1,9 @@
+//Final class
+
+//If you don't want the ability to override existing attribute values, declare attributes as final
+
+final class Example_7 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
