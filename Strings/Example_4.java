a7556ea7904f37c06305a1232732c5ea8a7eb57a
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 03:40:10 +0200

    Strings - Example 4

---
+++ Strings/Example_4.java
@@ -0,0 +1,10 @@
+//Using methods to convert strings to uppercase and lowercase
+
+public class Example_4 {
+    public static void main(String[] args) {
+        String txt = "Hello World";
+
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());
+    }
+}
