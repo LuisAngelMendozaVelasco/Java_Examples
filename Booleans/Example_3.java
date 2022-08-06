16933fdcc28c4675d90a6d53d0284dc1f932c5ab
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 04:18:40 +0200

    Booleans - Example 3

---
+++ Booleans/Example_3.java
@@ -0,0 +1,9 @@
+//Use the "equal to" operator to evaluate a boolean expression
+
+public class Example_3 {
+    public static void main(String[] args) {
+        int x = 10;
+        
+        System.out.println(x == 10); // returns true, because the value of x is equal to 10
+    }
+}
