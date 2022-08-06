b977bbe1d5b16c62d2863f9ac27239e46f22df16
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 17:35:40 +0200

    Arrays - Example 6

---
+++ Arrays/Example_6.java
@@ -0,0 +1,11 @@
+//Loop through an array with for-each
+
+public class Example_6 {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        for(String i : cars) {
+            System.out.println(i);
+        }    
+    }
+}
