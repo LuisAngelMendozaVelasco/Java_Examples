d379234102d4335091a37f7dbf157a1d82d2b7ef
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 17:18:31 +0200

    Loops - Example_6

---
+++ Loops/Example_6.java
@@ -0,0 +1,12 @@
+//Continue a loop
+
+public class Example_6 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            if(i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        }  
+    }
+}
