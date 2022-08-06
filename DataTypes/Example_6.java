387aceb2c928019cf96abdaa62564a68079775e7
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 02:05:00 +0200

    DataTypes - Example 6

---
+++ DataTypes/Example_6.java
@@ -0,0 +1,9 @@
+//Create a float type
+
+public class Example_6 {
+    public static void main(String[] args) {
+        float myNum = 5.75f; //Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+
+        System.out.println(myNum);  
+    }
+}
