f4d6eb7bcdea671c04788a85a87c19bbb7cdc2ac
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 17:23:21 +0200

    Arrays - Example 1

---
+++ Arrays/Example_1.java
@@ -0,0 +1,9 @@
+//Create and access an array
+
+public class Example_1 {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        
+        System.out.println(cars[0]);
+    }
+}
