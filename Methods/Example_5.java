910f6dd819db621954c49aabdfd51d2c5983ea47
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 21:46:42 +0200

    Methods - Example 5

---
+++ Methods/Example_5.java
@@ -0,0 +1,11 @@
+//Return value
+
+public class Example_5 {
+    static int myMethod(int x) {
+        return 5 + x;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(myMethod(3));
+    }
+}
