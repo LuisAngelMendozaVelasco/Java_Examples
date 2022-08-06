37c8a2b7a560d220f7aabcc4111d51b2a10900a7
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 02:06:56 +0200

    DataTypes - Example 7

---
+++ DataTypes/Example_7.java
@@ -0,0 +1,9 @@
+//Create a double type
+
+public class Example_7 {
+    public static void main(String[] args) {
+        double myNum = 19.99d; //Stores fractional numbers. Sufficient for storing 15 decimal digits
+
+        System.out.println(myNum);  
+    }
+}
