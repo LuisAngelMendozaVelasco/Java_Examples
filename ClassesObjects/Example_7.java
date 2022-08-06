1afd3c74872444b73789830b6303c0046fadf128
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 23:05:52 +0200

    ClassesObjects - Example 7

---
+++ ClassesObjects/Example_7.java
@@ -0,0 +1,11 @@
+//Create a class method
+
+public class Example_7 {
+    static void myMethod() {
+        System.out.println("Hello World!");
+    }
+    
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
