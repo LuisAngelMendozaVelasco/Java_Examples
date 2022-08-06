881d77d7a8c2dc9da240703ac6bca42cf717c77d
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 03:50:59 +0200

    Math - Example 2

---
+++ Math/Example_2.java
@@ -0,0 +1,7 @@
+//Math.min(x,y) - return the lowest value of x and y
+
+public class Example_2 {
+    public static void main(String[] args) {
+        System.out.println(Math.min(5, 10));  
+    }
+}
