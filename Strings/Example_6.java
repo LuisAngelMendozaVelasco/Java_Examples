58b976264a7a041d478f4d9c926d4cfa7e30a561
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 03:44:22 +0200

    Strings - Example 6

---
+++ Strings/Example_6.java
@@ -0,0 +1,10 @@
+//String concatenation
+
+public class Example_6 {
+    public static void main(String args[]) {
+        String firstName = "Luis";
+        String lastName = "Mendoza";
+
+        System.out.println(firstName + " " + lastName);
+    }
+}
