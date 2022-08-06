0d2df2c8d138c005b7cb7d52034bd062ba341e5e
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 22:38:40 +0200

    ClassesObjects - Example 2

---
+++ ClassesObjects/Example_2.java
@@ -0,0 +1,13 @@
+//Create multiple objects of a class
+
+public class Example_2 {
+    int x = 5;
+
+    public static void main(String[] args) {
+        Example_2 myObj1 = new Example_2();
+        Example_2 myObj2 = new Example_2();
+
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+    }
+}
