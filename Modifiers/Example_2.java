96d62d3c327fa5b073ad4dcd7bd85707739123fa
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 23:54:14 +0200

    Modifiers - Example 2

---
+++ Modifiers/Example_2.java
@@ -0,0 +1,7 @@
+//Default class
+
+class Example_2 { // The class is only accessible by classes in the same package. This is used when you don't specify a modifier.
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
