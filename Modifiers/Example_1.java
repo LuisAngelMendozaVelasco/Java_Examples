cd8d8204b7b82eaaadc180478445dc5dc2eec8ec
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 23:50:12 +0200

    Modifiers - Example 1

---
+++ Modifiers/Example_1.java
@@ -0,0 +1,12 @@
+//Public class
+
+/*The public keyword is an access modifier, meaning that it is used to set the access level for classes, attributes, methods and constructors.
+We divide modifiers into two groups:
+    Access Modifiers - controls the access level
+    Non-Access Modifiers - do not control access level, but provides other functionality*/
+
+public class Example_1 { // The class is accessible by any other class
+    public static void main(String[] args) { 
+        System.out.println("Hello World");
+    }
+}
