37f9487e35bc7b9398c1767c8d6de1ca527e2a34
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 03:18:49 +0200

    TypeCasting - Example 2

---
+++ TypeCasting/Example_2.java
@@ -0,0 +1,11 @@
+//Narrowing casting
+
+public class Example_2 {
+    public static void main(String[] args) {
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble; // Explicit casting: double to int
+    
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+}
