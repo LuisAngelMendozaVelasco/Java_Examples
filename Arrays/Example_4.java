4d17f3e29469d36742a7c66e5ba8be22abcfe105
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 17:30:34 +0200

    Arrays - Example 4

---
+++ Arrays/Example_4.java
@@ -0,0 +1,10 @@
+//Access an array
+
+public class Example_4 {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        
+        cars[0] = "Opel";
+        System.out.println(cars[0]);
+    }
+}
