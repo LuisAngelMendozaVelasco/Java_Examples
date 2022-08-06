7eff4961e30ea945e5e4c1e80e8e92ac9ee9b621
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 00:08:20 +0200

    Modifiers - Example 3

---
+++ Modifiers/Example_3.java
@@ -0,0 +1,11 @@
+//Public attributes
+
+public class Example_3 {
+    public static void main(String[] args) {
+        Example_3_Person myObj = new Example_3_Person(); 
+        
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+        System.out.println("Email: " + myObj.email);
+        System.out.println("Age: " + myObj.age);
+    }
+}
---
+++ Modifiers/Example_3_Person.java
@@ -0,0 +1,8 @@
+//public	The code is accessible for all classes
+
+public class Example_3_Person {
+    public String fname = "Luis";
+    public String lname = "Mendoza";
+    public String email = "luis@mendoza.com";
+    public int age = 24;
+}
