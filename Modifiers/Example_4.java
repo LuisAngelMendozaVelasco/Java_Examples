8fd8f8ac447b7a6d99c31d0c81975ff1e211bee1
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 00:57:49 +0200

    Modifiers - Example 4

---
+++ Modifiers/Example_4.java
@@ -0,0 +1,17 @@
+//Private attributes
+
+public class Example_4 {
+    // private	The code is only accessible within the declared class
+    private String fname = "Luis";
+    private String lname = "Mendoza";
+    private String email = "luis@mendoza.com";
+    private int age = 24;
+    
+    public static void main(String[] args) {
+        Example_4 myObj = new Example_4();
+
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+        System.out.println("Email: " + myObj.email);
+        System.out.println("Age: " + myObj.age);
+    }
+}
