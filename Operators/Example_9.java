152d994aeccfe964e0e6b10e6c548f2c869ba51c
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 03:32:03 +0200

    Operators - Example 9

---
+++ Operators/Example_9.java
@@ -0,0 +1,10 @@
+//Comparison operator
+
+public class Example_9 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+
+        System.out.println(x == y); // returns false because 5 is not equal to 3
+    }
+}
