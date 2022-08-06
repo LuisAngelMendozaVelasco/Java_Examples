86e3e4fd57acd2f0a720435684955e55e9232868
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 04:24:19 +0200

    Conditions - Example 3

---
+++ Conditions/Example_3.java
@@ -0,0 +1,17 @@
+//The else if statement
+
+public class Example_3 {
+    public static void main(String[] args) {
+        int time = 22;
+        
+        if(time < 10) {
+            System.out.println("Good morning.");
+        } 
+        else if(time < 20) {
+            System.out.println("Good day.");
+        }  
+        else {
+            System.out.println("Good evening.");
+        }
+    }
+}
