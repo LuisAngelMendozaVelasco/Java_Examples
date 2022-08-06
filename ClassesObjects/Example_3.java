e74a5b1fac585fbef01f3ee010bc6144bec902d5
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-06 22:41:09 +0200

    ClassesObjects - Example 3

---
+++ ClassesObjects/Example_3.java
@@ -0,0 +1,11 @@
+//Accessing class attributes (variables)
+
+public class Example_3 {
+    int x = 5;
+
+    public static void main(String[] args) {
+        Example_3 myObj = new Example_3();
+
+        System.out.println(myObj.x);
+    }
+}
