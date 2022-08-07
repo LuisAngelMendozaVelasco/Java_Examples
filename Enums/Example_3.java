cb9d45cb6937b07443220715531dd4c310f95c41
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 17:06:49 +0200

    Enums - Example 3

---
+++ Enums/Example_3.java
@@ -0,0 +1,26 @@
+//An enum in a switch statement
+
+enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+}
+  
+
+public class Example_3 {
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM; 
+                    
+        switch(myVar) {
+            case LOW:
+                System.out.println("Low level");
+                break;
+            case MEDIUM:
+                System.out.println("Medium level");
+                break;
+            case HIGH:
+                System.out.println("High level");
+                break;
+        }
+    }
+}
