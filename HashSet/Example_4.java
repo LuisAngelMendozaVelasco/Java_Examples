0e42d6e0abb56844e2ba2d867ed48c3c20f210fb
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 20:18:42 +0200

    HashSet - Example 4

---
+++ HashSet/Example_4.java
@@ -0,0 +1,18 @@
+//Remove all items from a HashSet
+
+// Import the HashSet class
+import java.util.HashSet;
+
+public class Example_4 {
+    public static void main(String[] args) {
+        HashSet<String> cars = new HashSet<String>();
+        
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("BMW");
+        cars.add("Mazda");
+        cars.clear();
+        System.out.println(cars);
+    }
+}
