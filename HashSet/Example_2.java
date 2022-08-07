e391714c6e06e161f54977275585c3f433fe37b2
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 20:15:16 +0200

    HashSet - Example 2

---
+++ HashSet/Example_2.java
@@ -0,0 +1,17 @@
+//Check if an item exists in a HashSet
+
+// Import the HashSet class
+import java.util.HashSet;
+
+public class Example_2 {
+    public static void main(String[] args) {
+        HashSet<String> cars = new HashSet<String>();
+        
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("BMW");
+        cars.add("Mazda");
+        System.out.println(cars.contains("Mazda"));
+    }
+}
