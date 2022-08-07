6fe0b57b42727a7230f64130c1634f499348b032
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 20:20:08 +0200

    HashSet - Example 5

---
+++ HashSet/Example_5.java
@@ -0,0 +1,17 @@
+//Get the size of a HashSet
+
+// Import the HashSet class
+import java.util.HashSet;
+
+public class Example_5 {
+    public static void main(String[] args) {
+        HashSet<String> cars = new HashSet<String>();
+        
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("BMW");
+        cars.add("Mazda");
+        System.out.println(cars.size());
+    }
+}
