3510369c3498ee6b8717904b9641efa88f861e4e
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 17:31:04 +0200

    Dates - Example 2

---
+++ Dates/Example_2.java
@@ -0,0 +1,11 @@
+//Display current time
+
+import java.time.LocalTime;  // import the LocalTime class
+
+public class Example_2 {
+    public static void main(String[] args) {  
+        LocalTime myObj = LocalTime.now();
+
+        System.out.println(myObj);
+    } 
+}
