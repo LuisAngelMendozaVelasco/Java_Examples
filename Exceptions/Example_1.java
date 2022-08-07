b4fad28ece2bb998cedb05161bc3b69c5d13ef35
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 21:24:40 +0200

    Exceptions - Example 1

---
+++ Exceptions/Example_1.java
@@ -0,0 +1,14 @@
+//The try...catch statement
+
+public class Example_1 {
+    public static void main(String[] args) {
+        try {
+            int[] myNumbers = {1, 2, 3};
+            
+            System.out.println(myNumbers[10]);
+        } 
+        catch(Exception e) {
+            System.out.println("Something went wrong.");
+        }
+    }
+}
