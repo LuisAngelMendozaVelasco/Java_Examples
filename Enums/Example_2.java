077ff170c714654183edd5ce559d906fd267d491
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 17:04:56 +0200

    Enums - Example 2

---
+++ Enums/Example_2.java
@@ -0,0 +1,15 @@
+//An enum inside a class
+
+public class Example_2 {
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    
+    public static void main(String[] args) { 
+        Level myVar = Level.MEDIUM; 
+
+        System.out.println(myVar); 
+    } 
+}
