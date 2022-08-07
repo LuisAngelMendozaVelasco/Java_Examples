73a2f708adec3dcfe9906ae47ff60d629c20305b
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 19:00:25 +0200

    HashMap - Example 4

---
+++ HashMap/Example_4.java
@@ -0,0 +1,16 @@
+//Remove all items from a HashMap
+
+import java.util.HashMap;
+
+public class Example_4 {
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+        capitalCities.clear();
+        System.out.println(capitalCities); 
+    }
+}
