fdd4073ad9fd80b365ba9bab4d715cc78e6c26f9
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 22:21:26 +0200

    Threads - Example 2

---
+++ Threads/Example_2.java
@@ -0,0 +1,15 @@
+//Running a thread by implementing the Runnable interface
+
+public class Example_2 implements Runnable {
+    public static void main(String[] args) {
+        Example_2 obj = new Example_2();
+        Thread thread = new Thread(obj);
+        
+        thread.start();
+        System.out.println("This code is outside of the thread");
+    }
+
+    public void run() {
+        System.out.println("This code is running in a thread");
+    }
+}
