1c7f0f4f0b6ea983d72cc6b00d0232742a60417f
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 18:32:22 +0200

    ArrayList - Example 6

---
+++ ArrayList/Example_6.java
@@ -0,0 +1,17 @@
+//Loop through an ArrayList
+
+import java.util.ArrayList;
+
+public class Example_6 {
+    public static void main(String[] args) { 
+        ArrayList<String> cars = new ArrayList<String>();
+        
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        for (int i = 0; i < cars.size(); i++) {
+            System.out.println(cars.get(i));
+        }
+    }
+}
