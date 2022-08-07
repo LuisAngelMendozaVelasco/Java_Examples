e76733378279b563af88ab48fe294e0575a2d100
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 21:19:45 +0200

    WrapperClasses - Example 4

---
+++ WrapperClasses/Example_4.java
@@ -0,0 +1,10 @@
+//Convert wrapper objects to strings
+
+public class Example_4 {
+    public static void main(String[] args) { 
+        Integer myInt = 100; 
+        String myString = myInt.toString();
+        
+        System.out.println(myString.length());
+    }
+}
