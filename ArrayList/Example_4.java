fb514e0e361271a503c176afe26ed6f7be8cc7e7
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 18:25:58 +0200

    ArrayList - Example 4

---
+++ ArrayList/Example_4.java
@@ -0,0 +1,16 @@
+//Remove all items from an ArrayList
+
+import java.util.ArrayList;
+
+public class Example_4 {
+    public static void main(String[] args) { 
+        ArrayList<String> cars = new ArrayList<String>();
+        
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        cars.clear();
+        System.out.println(cars);
+    } 
+}
