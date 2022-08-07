64b8e2aa90a6f10d2ef67695e5a833ae1fc4cb23
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 17:02:35 +0200

    Enums - Example 1

---
+++ Enums/Example_1.java
@@ -0,0 +1,18 @@
+//Create an enum with a group of constants
+
+/*An enum is a special "class" that represents a group of constants (unchangeable variables, like final variables).
+To create an enum, use the enum keyword (instead of class or interface), and separate the constants with a comma. Note that they should be in uppercase letters*/
+
+enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+}
+
+public class Example_1 {
+    public static void main(String[] args) { 
+        Level myVar = Level.MEDIUM; 
+
+        System.out.println(myVar); 
+    } 
+}
