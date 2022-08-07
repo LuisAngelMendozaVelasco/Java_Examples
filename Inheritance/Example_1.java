647ac1c28fd3990a7b15b303e2d5207fe9a7ba10
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 04:31:06 +0200

    Inheritance - Example 1

---
+++ Inheritance/Example_1.java
@@ -0,0 +1,20 @@
+//Java inheritance
+
+class Vehicle {
+    protected String brand = "Ford";
+
+    public void honk() {
+        System.out.println("Tuut, tuut!");
+    }
+}
+  
+class Example_1 extends Vehicle {
+    private String modelName = "Mustang";
+    
+    public static void main(String[] args) {
+        Example_1 myFastCar = new Example_1();
+        
+        myFastCar.honk();
+        System.out.println(myFastCar.brand + " " + myFastCar.modelName);
+    }
+}
