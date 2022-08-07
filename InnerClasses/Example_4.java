69c9a0309e609657cd491c6fe9711a1bcfa5d683
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 16:38:59 +0200

    InnerClasses - Example 4

---
+++ InnerClasses/Example_4.java
@@ -0,0 +1,22 @@
+//Access outer class from inner class
+
+//One advantage of inner classes, is that they can access attributes and methods of the outer class
+
+class OuterClass {
+    int x = 10; 
+  
+    class InnerClass {
+        public int myInnerMethod() {
+            return x;
+        }
+    }
+}
+
+public class Example_4 {
+    public static void main(String[] args) {
+        OuterClass myOuter = new OuterClass();
+        OuterClass.InnerClass myInner = myOuter.new InnerClass();
+
+        System.out.println(myInner.myInnerMethod());
+    }
+}
