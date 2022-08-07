c5c3a4ae4eefb8111e57fb8638d63106ac6d5da5
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-07 19:04:14 +0200

    HashMap - Example 6

---
+++ HashMap/Example_6.java
@@ -0,0 +1,17 @@
+//Loop through a HashMap
+
+import java.util.HashMap;
+
+public class Example_6 {
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+        for (String i : capitalCities.keySet()) {
+            System.out.println("key: " + i + " value: " + capitalCities.get(i));
+        }
+    }
+}
